2e2daa93841d029ae9c34a6ae8f07791c4c61cbb
Tim DeCillis <timdecillis@gmail.com>
2023-09-10 22:43:19 -0700

    Added reducer with all possible actions

---
+++ client/src/redux/reducer.js
@@ -0,0 +1,46 @@
+import * as types from './actionTypes';
+
+const initialState = {
+  user: null,
+  songs: [],
+  choices: [],
+  choiceArtist: null
+};
+
+export default function reducer(state = initialState, action) {
+  if (action.type === types.SONG_CHOICES_UPDATED) {
+    return {
+      ...state,
+      choices: action.payload.choices,
+      choiceArtist: actio.payload.artist,
+    };
+  } else if (action.type === types.SONG_ADDED) {
+    return {
+      ...state,
+      songs: [...state.songs, action.payload.song],
+    };
+  } else if (action.type === types.SONG_REMOVED) {
+    return {
+      ...state,
+      songs: state.songs.filter((song) =>
+        song !== action.payload.song
+      ),
+    };
+  } else if (action.type === types.SONG_COMPLETED) {
+    return {
+      ...state,
+      songs: state.songs.map((song) =>
+        song === action.payload.song ? {...song, completed: !song.completed} : song
+      ),
+    };
+  } else if (action.type === types.NOTES_UPDATED) {
+    return {
+      ...state,
+      songs: state.songs.map((song) =>
+        song === action.payload.song ? {...song, notes: action.payload.notes} : song
+      ),
+    };
+  } else {
+    return state;
+  }
+}

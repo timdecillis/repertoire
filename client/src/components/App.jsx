80306c467d4053c05f98b4c484ee3ca7b742dc8d
Tim DeCillis <timdecillis@gmail.com>
2023-09-12 20:41:42 -0700

    Added alt attribute for image

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -86,7 +86,7 @@ const App = () => {
       </div>
       {!signedIn ?
         <div className="landing">
-          <img className="painting" src="https://media.istockphoto.com/id/1220009855/vector/a-young-man-playing-guitar-at-home-guitarist-musician-is-sitting-in-quarantine-alone-flat.jpg?s=612x612&w=0&k=20&c=p9bINE_TxSd4G4VRkvaHffh5vZjUdVUQvXGcqBlDrSs="></img>
+          <img className="painting" src="https://media.istockphoto.com/id/1220009855/vector/a-young-man-playing-guitar-at-home-guitarist-musician-is-sitting-in-quarantine-alone-flat.jpg?s=612x612&w=0&k=20&c=p9bINE_TxSd4G4VRkvaHffh5vZjUdVUQvXGcqBlDrSs=" alt="a man playing guitar with his cat"></img>
           <HomeButtons oneOpen={oneOpen} setOneOpen={setOneOpen} setSignInOpen={setSignInOpen} setCreateOpen={setCreateOpen} />
 
         </div>

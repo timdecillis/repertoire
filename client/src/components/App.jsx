28b9f31a46d43f3d1b32566990a4a0c95869c2dd
Tim DeCillis <timdecillis@gmail.com>
2023-09-12 21:14:56 -0700

    Minor refactor

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -81,31 +81,28 @@ const App = () => {
 
   return (
     <div className="container">
+
       <div className="banner">
         <div className="header">Repertoire</div>
-
       </div>
 
       {!signedIn ?
-
         <LandingPage signInOpen={signInOpen} oneOpen={oneOpen} setOneOpen={setOneOpen} setSignInOpen={setSignInOpen} createOpen={createOpen} setCreateOpen={setCreateOpen}/>
         :
         <button className="sign-in-button" onClick={userSignOut}>Sign Out</button>}
 
       <CreateUser setAuthUser={setAuthUser} setCreateOpen={setCreateOpen} createOpen={createOpen} setOneOpen={setOneOpen} setSignedIn={setSignedIn} />
 
-
       <SignIn authUser={authUser} getSongs={getSongs} email={email} password={password} setAuthUser={setAuthUser} setSignedIn={setSignedIn} setEmail={setEmail}
         setPassword={setPassword} signInOpen={signInOpen}
         setSignInOpen={setSignInOpen} />
 
       <div className="list-and-dash">
-
         <Dashboard signedIn={signedIn} addSong={addSong} band={band} authUser={authUser} setBand={setBand} songs={songs} setSongs={setSongs} choices={choices} handleSearch={handleSearch} />
 
         {songs.length > 0 && <SongList authUser={authUser} signedIn={signedIn} handleNotes={handleNotes} handleCompleted={handleCompleted} deleteSong={deleteSong} band={band} songs={songs} setSongs={setSongs} />}
-
       </div>
+
     </div>
   );
 };

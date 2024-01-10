541ab21fa95b20130d3abdcf88c9b94a291976ba
Tim DeCillis <timdecillis@gmail.com>
2024-01-09 18:06:12 -0800

    added type submit

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -27,6 +27,82 @@ const Dashboard = ({
     <>
       <div className="dashboard">
         <div className="add-song">Find a song</div>
+        <form
+          onSubmit={() => {
+            handleSearch(band, instrument, difficulty);
+          }}
+        >
+          <div className="instrument">
+            <div className="dash-option">Instrument</div>
+            <input
+              onClick={(e) => setInstrument(e.target.value)}
+              type="radio"
+              name="instrument"
+              value="guitar"
+            ></input>
+            <label className="radio-button" htmlFor="instrument">
+              Guitar
+            </label>
+            <input
+              onClick={(e) => setInstrument(e.target.value)}
+              type="radio"
+              name="instrument"
+              value="drums"
+            ></input>
+            <label className="radio-button" htmlFor="instrument">
+              Drums
+            </label>
+            <input
+              onClick={(e) => setInstrument(e.target.value)}
+              type="radio"
+              name="instrument"
+              value="piano"
+            ></input>
+            <label className="radio-button" htmlFor="instrument">
+              Piano
+            </label>
+          </div>
+
+          <div className="difficulty">
+            <div className="dash-option">Difficulty</div>
+            <input
+              onClick={(e) => setDifficulty(e.target.value)}
+              type="radio"
+              name="difficulty"
+              value="beginner"
+            ></input>
+            <label className="radio-button" htmlFor="difficulty">
+              Beginner
+            </label>
+            <input
+              onClick={(e) => setDifficulty(e.target.value)}
+              type="radio"
+              name="difficulty"
+              value="intermediate"
+            ></input>
+            <label className="radio-button" htmlFor="difficulty">
+              Intermediate
+            </label>
+            <input
+              onClick={(e) => setDifficulty(e.target.value)}
+              type="radio"
+              name="difficulty"
+              value="advanced"
+            ></input>
+            <label className="radio-button" htmlFor="difficulty">
+              Advanced
+            </label>
+          </div>
+
+          <div className="search">
+            <input
+              className="input"
+              placeholder="Enter an artist or band"
+              onChange={(e) => setBand(e.target.value)}
+            ></input>
+          </div>
+          <input className="sign-in-button" type="submit" value="Search" />
+        </form>
 
         {/* <Instrument instrument={instrument} setInstrument={setInstrument}></Instrument>
 
@@ -41,42 +117,6 @@ const Dashboard = ({
           authUser={authUser}
         />
       </div>
-
-      <form onSubmit={handleSearch}>
-
-        <div className="instrument">
-          <div className="dash-option">Instrument</div>
-          <input
-            onClick={(e) => setInstrument(e.target.value)}
-            type="radio"
-            name="instrument"
-            value="guitar"
-          ></input>
-          <label className="radio-button" htmlFor="instrument">
-            Guitar
-          </label>
-          <input
-            onClick={(e) => setInstrument(e.target.value)}
-            type="radio"
-            name="instrument"
-            value="drums"
-          ></input>
-          <label className="radio-button" htmlFor="instrument">
-            Drums
-          </label>
-          <input
-            onClick={(e) => setInstrument(e.target.value)}
-            type="radio"
-            name="instrument"
-            value="piano"
-          ></input>
-          <label className="radio-button" htmlFor="instrument">
-            Piano
-          </label>
-        </div>
-
-
-      </form>
     </>
   );
 };

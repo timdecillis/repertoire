6181ee18140a3ccbe0939da9e9516fc34ca105be
Tim DeCillis <timdecillis@gmail.com>
2024-01-09 17:40:38 -0800

    added new form

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -1,7 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const SignIn = ({ setSignedIn, setEmail, setSignInOpen, setPassword, signInOpen, setAuthUser, email, password, getSongs, authUser }) => {
-
+const SignIn = ({
+  setSignedIn,
+  setEmail,
+  setSignInOpen,
+  setPassword,
+  signInOpen,
+  setAuthUser,
+  email,
+  password,
+  getSongs,
+  authUser,
+}) => {
   useEffect(() => {
     if (authUser !== null) {
       setSignInOpen(false);
@@ -9,7 +19,6 @@ const SignIn = ({ setSignedIn, setEmail, setSignInOpen, setPassword, signInOpen,
       getSongs();
     }
   }, [authUser]);
-
 
   if (!signInOpen) {
     return null;
@@ -20,13 +29,20 @@ const SignIn = ({ setSignedIn, setEmail, setSignInOpen, setPassword, signInOpen,
   };
 
   return (
-    <div className="sign-in" >
-      <div className="inputs">
-        <input onChange={(e) => setEmail(e.target.value)} type="email" className="input" placeholder="email"></input>
-        <input onChange={(e) => setPassword(e.target.value)} type="password" className="input" placeholder="password"></input>
-      </div>
-      <button className="sign-in-button" onClick={handleSignIn}>Submit</button>
-    </div>
+    // <div className="sign-in" >
+    //   <div className="inputs">
+    //     <input onChange={(e) => setEmail(e.target.value)} type="email" className="input" placeholder="email"></input>
+    //     <input onChange={(e) => setPassword(e.target.value)} type="password" className="input" placeholder="password"></input>
+    //   </div>
+    //   <button className="sign-in-button" onClick={handleSignIn}>Submit</button>
+    // </div>
+    <form>
+      <label htmlFor="email">Please enter your email</label>
+      <input id="email" type="text" name="email" />
+      <label htmlFor="password">Please enter your password</label>
+      <input id="password" type="text" name="password" />
+      <input type="submit" value="login"/>
+    </form>
   );
 };
 

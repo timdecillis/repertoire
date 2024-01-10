2335ca0fe3e8c44b94d5f7ac3ed60de0421bf7c6
Tim DeCillis <timdecillis@gmail.com>
2024-01-09 17:59:49 -0800

    moved instrument form

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -24,53 +24,60 @@ const Dashboard = ({
   }
 
   return (
-    <div className="dashboard">
+    <>
+      <div className="dashboard">
+        <div className="add-song">Find a song</div>
 
-      <div className="add-song" >Find a song</div>
-
-      <Instrument instrument={instrument} setInstrument={setInstrument}></Instrument>
+        {/* <Instrument instrument={instrument} setInstrument={setInstrument}></Instrument>
 
       <Difficulty difficulty={difficulty} setDifficulty={setDifficulty} />
 
-      <Search band={band} setBand={setBand} instrument={instrument} difficulty={difficulty} handleSearch={handleSearch} />
+      <Search band={band} setBand={setBand} instrument={instrument} difficulty={difficulty} handleSearch={handleSearch} /> */}
 
-      <Choices choices={choices} addSong={addSong} band={band} authUser={authUser}/>
-
-    </div>
-    <div className="instrument">
-      <div className="dash-option" >Instrument</div>
-      <input onClick={(e) => setInstrument(e.target.value)} type="radio" name="instrument" value="guitar"></input>
-      <label className="radio-button" htmlFor="instrument">Guitar</label>
-      <input onClick={(e) => setInstrument(e.target.value)} type="radio" name="instrument" value="drums"></input>
-      <label className="radio-button" htmlFor="instrument">Drums</label>
-      <input onClick={(e) => setInstrument(e.target.value)} type="radio" name="instrument" value="piano"></input>
-      <label className="radio-button" htmlFor="instrument">Piano</label>
-    </div>
-    <form onSubmit={handleSearch} className="sign-in">
-      <div className="inputs">
-        <label className="input" htmlFor="email">
-          Please enter your email
-        </label>
-        <input
-          onChange={(e) => setEmail(e.target.value)}
-          className="input"
-          id="email"
-          type="text"
-          name="email"
+        <Choices
+          choices={choices}
+          addSong={addSong}
+          band={band}
+          authUser={authUser}
         />
-        <label className="input" htmlFor="password">
-          Please enter your password
-        </label>
-        <input
-          onChange={(e) => setPassword(e.target.value)}
-          className="input"
-          id="password"
-          type="text"
-          name="password"
-        />
-        <input className="input" type="submit" value="Search" />
       </div>
-    </form>
+
+      <form onSubmit={handleSearch}>
+
+        <div className="instrument">
+          <div className="dash-option">Instrument</div>
+          <input
+            onClick={(e) => setInstrument(e.target.value)}
+            type="radio"
+            name="instrument"
+            value="guitar"
+          ></input>
+          <label className="radio-button" htmlFor="instrument">
+            Guitar
+          </label>
+          <input
+            onClick={(e) => setInstrument(e.target.value)}
+            type="radio"
+            name="instrument"
+            value="drums"
+          ></input>
+          <label className="radio-button" htmlFor="instrument">
+            Drums
+          </label>
+          <input
+            onClick={(e) => setInstrument(e.target.value)}
+            type="radio"
+            name="instrument"
+            value="piano"
+          ></input>
+          <label className="radio-button" htmlFor="instrument">
+            Piano
+          </label>
+        </div>
+
+
+      </form>
+    </>
   );
 };
 

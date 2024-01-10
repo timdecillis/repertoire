c4a693b8acd0771f393f9fb0cc89df57a26dc334
Tim DeCillis <timdecillis@gmail.com>
2024-01-09 17:51:26 -0800

    added new form for search

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,14 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Instrument from './Instrument.jsx';
-import Difficulty from './Difficulty.jsx';
-import Search from './Search.jsx';
-import Choices from './Choices.jsx';
+import Instrument from "./Instrument.jsx";
+import Difficulty from "./Difficulty.jsx";
+import Search from "./Search.jsx";
+import Choices from "./Choices.jsx";
 
-const Dashboard = ({ handleSearch, choices, songs, setSongs, band, setBand, addSong, signedIn, authUser }) => {
-
-  const [instrument, setInstrument] = useState('');
-  const [difficulty, setDifficulty] = useState('');
+const Dashboard = ({
+  handleSearch,
+  choices,
+  songs,
+  setSongs,
+  band,
+  setBand,
+  addSong,
+  signedIn,
+  authUser,
+}) => {
+  const [instrument, setInstrument] = useState("");
+  const [difficulty, setDifficulty] = useState("");
 
   if (!signedIn) {
     return null;
@@ -28,6 +37,40 @@ const Dashboard = ({ handleSearch, choices, songs, setSongs, band, setBand, addS
       <Choices choices={choices} addSong={addSong} band={band} authUser={authUser}/>
 
     </div>
+    <div className="instrument">
+      <div className="dash-option" >Instrument</div>
+      <input onClick={(e) => setInstrument(e.target.value)} type="radio" name="instrument" value="guitar"></input>
+      <label className="radio-button" htmlFor="instrument">Guitar</label>
+      <input onClick={(e) => setInstrument(e.target.value)} type="radio" name="instrument" value="drums"></input>
+      <label className="radio-button" htmlFor="instrument">Drums</label>
+      <input onClick={(e) => setInstrument(e.target.value)} type="radio" name="instrument" value="piano"></input>
+      <label className="radio-button" htmlFor="instrument">Piano</label>
+    </div>
+    <form onSubmit={handleSearch} className="sign-in">
+      <div className="inputs">
+        <label className="input" htmlFor="email">
+          Please enter your email
+        </label>
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          className="input"
+          id="email"
+          type="text"
+          name="email"
+        />
+        <label className="input" htmlFor="password">
+          Please enter your password
+        </label>
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          className="input"
+          id="password"
+          type="text"
+          name="password"
+        />
+        <input className="input" type="submit" value="Search" />
+      </div>
+    </form>
   );
 };
 

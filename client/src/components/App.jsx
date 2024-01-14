8860c70431db7858e12ff09a01e3075cc32f4de9
Tim DeCillis <timdecillis@gmail.com>
2024-01-13 18:51:47 -0800

    removed old signout

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,23 +1,22 @@
-import React, { useState } from 'react';
-import Dashboard from './Dashboard.jsx';
-import SongList from './SongList.jsx';
-const axios = require('axios');
+import React, { useState } from "react";
+import Dashboard from "./Dashboard.jsx";
+import SongList from "./SongList.jsx";
+const axios = require("axios");
 
-import SignIn from './SignIn.jsx';
-import CreateUser from './CreateUser.jsx';
-import HomeButtons from './HomeButtons.jsx';
-import LandingPage from './LandingPage.jsx';
+import SignIn from "./SignIn.jsx";
+import CreateUser from "./CreateUser.jsx";
+import HomeButtons from "./HomeButtons.jsx";
+import LandingPage from "./LandingPage.jsx";
 
 const App = () => {
-
   const [songs, setSongs] = useState([]);
   const [choices, setChoices] = useState([]);
-  const [band, setBand] = useState('');
+  const [band, setBand] = useState("");
   const [signedIn, setSignedIn] = useState(false);
   const [signInOpen, setSignInOpen] = useState(false);
   const [createOpen, setCreateOpen] = useState(false);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [oneOpen, setOneOpen] = useState(true);
   const [authUser, setAuthUser] = useState(null);
 
@@ -29,17 +28,23 @@ const App = () => {
   };
 
   const getSongs = () => {
-    return axios.get('/getSongs', { params: { email: authUser } })
+    return axios
+      .get("/getSongs", { params: { email: authUser } })
       .then(({ data }) => {
         setSongs(data);
       });
   };
 
   const handleSearch = (band, instrument, difficulty) => {
-    return axios.post('/findBand', { band: band, instrument: instrument, difficulty: difficulty })
+    return axios
+      .post("/findBand", {
+        band: band,
+        instrument: instrument,
+        difficulty: difficulty,
+      })
       .then(({ data }) => {
-        if (data.songs[0] === 'song 1') {
-          setChoices(['Your search did not match any results :(']);
+        if (data.songs[0] === "song 1") {
+          setChoices(["Your search did not match any results :("]);
         } else {
           setChoices(data.songs);
           setBand(data.artist);
@@ -48,27 +53,38 @@ const App = () => {
   };
 
   const addSong = (authUser, song, band) => {
-    return axios.post('/addSong', { email: authUser, song: song, artist: band })
+    return axios
+      .post("/addSong", { email: authUser, song: song, artist: band })
       .then(() => {
         getSongs();
       });
   };
 
   const deleteSong = (authUser, song, artist) => {
-    return axios.delete('/deleteSong', { data: { email: authUser, song: song, artist: artist } })
+    return axios
+      .delete("/deleteSong", {
+        data: { email: authUser, song: song, artist: artist },
+      })
       .then(() => {
         getSongs();
       });
   };
 
   const handleCompleted = (authUser, song, artist) => {
-    return axios.put('/updateSong', { email: authUser, song: song, artist: artist })
+    return axios
+      .put("/updateSong", { email: authUser, song: song, artist: artist })
       .then(() => {
         getSongs();
       });
   };
   const handleNotes = (authUser, song, artist, notes) => {
-    return axios.put('/updateNotes', { email: authUser, song: song, artist: artist, notes: notes })
+    return axios
+      .put("/updateNotes", {
+        email: authUser,
+        song: song,
+        artist: artist,
+        notes: notes,
+      })
       .then(() => {
         getSongs();
       });
@@ -76,23 +92,53 @@ const App = () => {
 
   return (
     <div className="container">
-
       <div className="banner">
         <div className="header">Repertoire</div>
         {signedIn && <span className="sign-out-button">Sign Out</span>}
       </div>
 
-      {!signedIn ?
-        <LandingPage setSignedIn={setSignedIn} email={email} getSongs={getSongs} setAuthUser={setAuthUser} authUser={authUser} signInOpen={signInOpen} oneOpen={oneOpen} setOneOpen={setOneOpen} setSignInOpen={setSignInOpen} createOpen={createOpen} setCreateOpen={setCreateOpen}/>
-        :
-        <button className="sign-in-button" onClick={userSignOut}>Sign Out</button>}
+      {!signedIn && (
+        <LandingPage
+          setSignedIn={setSignedIn}
+          email={email}
+          getSongs={getSongs}
+          setAuthUser={setAuthUser}
+          authUser={authUser}
+          signInOpen={signInOpen}
+          oneOpen={oneOpen}
+          setOneOpen={setOneOpen}
+          setSignInOpen={setSignInOpen}
+          createOpen={createOpen}
+          setCreateOpen={setCreateOpen}
+        />
+      )}
 
       <div className="list-and-dash">
-        <Dashboard signedIn={signedIn} addSong={addSong} band={band} authUser={authUser} setBand={setBand} songs={songs} setSongs={setSongs} choices={choices} handleSearch={handleSearch} />
+        <Dashboard
+          signedIn={signedIn}
+          addSong={addSong}
+          band={band}
+          authUser={authUser}
+          setBand={setBand}
+          songs={songs}
+          setSongs={setSongs}
+          choices={choices}
+          handleSearch={handleSearch}
+        />
 
-        {songs.length > 0 && <SongList authUser={authUser} signedIn={signedIn} handleNotes={handleNotes} handleCompleted={handleCompleted} deleteSong={deleteSong} band={band} songs={songs} setSongs={setSongs} />}
+        {songs.length > 0 && (
+          <SongList
+            authUser={authUser}
+            signedIn={signedIn}
+            handleNotes={handleNotes}
+            handleCompleted={handleCompleted}
+            deleteSong={deleteSong}
+            band={band}
+            songs={songs}
+            setSongs={setSongs}
+          />
+        )}
       </div>
-
     </div>
   );
 };

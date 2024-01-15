04ff677c676a08c85e130bbbdcf10936efd2dfa2
Tim DeCillis <timdecillis@gmail.com>
2024-01-14 16:41:18 -0800

    updated notes

--- client/src/components/Notes.jsx
+++ client/src/components/Notes.jsx
@@ -1,14 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import RemoveButton from './RemoveButton.jsx';
 
 const Notes = ({ setDraftOpen, song }) => {
-
   return (
     <div>
+      <div className="artist">by {song.artist}</div>
+      <div className="notes-heading">Notes</div>
+      {draftOpen ? (
+        <Draft
+          setDraft={setDraft}
+          setDraftOpen={setDraftOpen}
+          handleNotes={handleNotes}
+          authUser={authUser}
+          draft={draft}
+          song={song}
+        />
+      ) : (
+        <Notes song={song} setDraftOpen={setDraftOpen} />
+      )}
+      <Completed
+        handleCompleted={handleCompleted}
+        authUser={authUser}
+        song={song}
+      />
+      <RemoveButton deleteSong={deleteSong} authUser={authUser} song={song} />
       <div className="notes-container">
         <div className="notes">{song.notes}</div>
       </div>
       <button className="add-notes" onClick={() => setDraftOpen(true)}>
-        Add a note</button>
+        Add a note
+      </button>
     </div>
   );
 };

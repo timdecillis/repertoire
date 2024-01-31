7a6ab884876d1e711d1f6257d73bf7b914b90a38
Tim DeCillis <timdecillis@gmail.com>
2024-01-30 21:52:03 -0800

    formatted component

--- client/src/components/Choices.jsx
+++ client/src/components/Choices.jsx
@@ -1,13 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Choices = ({ choices, addSong, authUser, band, setDashOpen }) => {
-
   return (
     <div className="choices">
-      {choices.length > 0 && <div>{choices.map((choice, i) => <div className="choice" onClick={() => {
-        addSong(authUser, choice, band);
-        setDashOpen(false);
-      }} key={i}>{choice}</div>)}</div>}
+      {choices.length > 0 && (
+        <div>
+          {choices.map((choice, i) => (
+            <div
+              className="choice"
+              onClick={() => {
+                addSong(authUser, choice, band);
+                setDashOpen(false);
+              }}
+              key={i}
+            >
+              {choice}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };

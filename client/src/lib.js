3553d1efd53859cd1a365ff27b2d3760af5589dc
Tim DeCillis <timdecillis@gmail.com>
2024-02-01 15:18:12 -0800

    added delete to lib

--- client/src/lib.js
+++ client/src/lib.js
@@ -23,5 +23,16 @@ module.exports = {
         difficulty: difficulty,
       })
       .then(({data}) => data);
+  },
+  deleteSong: (authUser, song, artist) => {
+    return axios
+      .delete("/deleteSong", {
+        data: { email: authUser, song: song, artist: artist },
+      })
+      .then(() => {
+        getSongs(authUser).then((songs) => {
+          setSongs(songs);
+        });
+      });
   }
 };

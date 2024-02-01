fbb2ef3b27461786bd1ba21026d3dc504ff58f3e
Tim DeCillis <timdecillis@gmail.com>
2024-01-31 18:43:33 -0800

    formatted controllers

--- server/controllers.js
+++ server/controllers.js
@@ -1,6 +1,13 @@
-const { Configuration, OpenAIApi } = require('openai');
+const { Configuration, OpenAIApi } = require("openai");
 
-const { getSongs, saveSong, deleteSong, updateSong, updateNotes, createUser } = require('./models.js');
+const {
+  getSongs,
+  saveSong,
+  deleteSong,
+  updateSong,
+  updateNotes,
+  createUser,
+} = require("./models.js");
 
 const configuration = new Configuration({
   apiKey: process.env.API_KEY,
@@ -13,8 +20,9 @@ module.exports = {
     if (!configuration.apiKey) {
       res.status(500).json({
         error: {
-          message: 'OpenAI API key not configured, please follow instructions in README.md',
-        }
+          message:
+            "OpenAI API key not configured, please follow instructions in README.md",
+        },
       });
       return;
     }
@@ -24,21 +32,26 @@ module.exports = {
     if (band.trim().length === 0) {
       res.status(400).json({
         error: {
-          message: 'Please enter a valid band',
-        }
+          message: "Please enter a valid band",
+        },
       });
       return;
     }
     try {
       const completion = await openai.createChatCompletion({
-        model: 'gpt-3.5-turbo',
+        model: "gpt-3.5-turbo",
         messages: [
-          { role: 'system', content: 'You are a helpful assistant.' },
-          { role: 'user', content: generatePrompt(band, instrument, difficulty) },
+          { role: "system", content: "You are a helpful assistant." },
+          {
+            role: "user",
+            content: generatePrompt(band, instrument, difficulty),
+          },
         ],
-        temperature: 0
+        temperature: 0,
       });
-      res.status(200).json(JSON.parse(completion.data.choices[0].message.content));
+      res
+        .status(200)
+        .json(JSON.parse(completion.data.choices[0].message.content));
     } catch (error) {
       if (error.response) {
         console.error(error.response.status, error.response.data);
@@ -47,30 +60,30 @@ module.exports = {
         console.error(`Error with OpenAI API request: ${error.message}`);
         res.status(500).json({
           error: {
-            message: 'An error occurred during your request.',
-          }
+            message: "An error occurred during your request.",
+          },
         });
       }
     }
   },
   createUser: (req, res) => {
-    let {email, password} = req.body;
+    let { email, password } = req.body;
     createUser(email, password)
       .then(() => {
         res.status(201).send();
       })
       .catch((err) => {
-        console.log('Error adding user:', err);
+        console.log("Error adding user:", err);
       });
   },
   getSongs: (req, res) => {
-    let {email} = req.query;
+    let { email } = req.query;
     getSongs(email)
-      .then(({songs}) => {
+      .then(({ songs }) => {
         res.status(200).send(songs);
       })
       .catch((err) => {
-        console.log('Error retrieving songs:', err);
+        console.log("Error retrieving songs:", err);
       });
   },
   addSong: (req, res) => {
@@ -79,7 +92,7 @@ module.exports = {
         res.status(201).send();
       })
       .catch((err) => {
-        console.log('Error adding song:', err);
+        console.log("Error adding song:", err);
       });
   },
   deleteSong: (req, res) => {
@@ -88,7 +101,7 @@ module.exports = {
         res.status(203).send();
       })
       .catch((err) => {
-        console.log('Error deleting song:', err);
+        console.log("Error deleting song:", err);
       });
   },
   updateSong: (req, res) => {
@@ -97,7 +110,7 @@ module.exports = {
         res.status(202).send();
       })
       .catch((err) => {
-        console.log('Error updating song:', err);
+        console.log("Error updating song:", err);
       });
   },
   updateNotes: (req, res) => {
@@ -106,13 +119,16 @@ module.exports = {
         res.status(202).send();
       })
       .catch((err) => {
-        console.log('Error updating notes:', err);
+        console.log("Error updating notes:", err);
       });
-  }
+  },
 };
 
-const generatePrompt = (band, instrument = 'guitar', difficulty = 'beginner') => {
-  const capitalizedBand =
-    band[0].toUpperCase() + band.slice(1).toLowerCase();
+const generatePrompt = (
+  band,
+  instrument = "guitar",
+  difficulty = "beginner"
+) => {
+  const capitalizedBand = band[0].toUpperCase() + band.slice(1).toLowerCase();
   return `You are an expert music teacher. You have been asked to provide 3 songs by ${band} for a ${difficulty}-level player to learn on the ${instrument}. Take into consideration tempo, song complexity, and technical difficulty. Return only a JSON object, with this format: {artist: ${band}, songs: ['song 1', 'song 2', 'song 3']}.`;
 };

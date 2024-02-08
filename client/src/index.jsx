5903278c11a0e60c4d13e82c2dbb520bc24eb926
Tim DeCillis <timdecillis@gmail.com>
2024-02-07 19:03:37 -0800

    moved App

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,6 @@
 import {render, createRoot} from 'react-dom/client';
 import React from 'react';
-import App from './components/App.jsx';
+import App from './components/App/App.jsx';
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);

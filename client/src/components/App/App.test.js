2aca1e32dd6fb2c8754ee7bf3e16cb1fc876a82a
Tim DeCillis <timdecillis@gmail.com>
2024-02-08 14:36:09 -0800

    imported landing page

--- client/src/components/App/App.test.js
+++ client/src/components/App/App.test.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 
 import App from './App.jsx';
+import LandingPage from '../LandingPage/LandingPage.jsx';
 
 describe('App Component', () => {
   it('includes the container, banner, and header', () => {
+    const signedIn = true;
     render(<App />);
     expect(document.getElementsByClassName('container', 'banner', 'header')).toBeTruthy();
   });

e3002fdb9cb10afa89965aee0b7b4a5ccc25dd2a
Tim DeCillis <timdecillis@gmail.com>
2024-02-08 14:34:07 -0800

    simplified test

--- client/src/components/App/App.test.js
+++ client/src/components/App/App.test.js
@@ -4,10 +4,7 @@ import { render } from '@testing-library/react';
 import App from './App.jsx';
 
 describe('App Component', () => {
-  it('renders without errors', () => {
-    render(<App />);
-  });
-  it('renders the header', () => {
+  it('includes the container, banner, and header', () => {
     render(<App />);
     expect(document.getElementsByClassName('container', 'banner', 'header')).toBeTruthy();
   });

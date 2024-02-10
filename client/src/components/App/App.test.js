34d1636f8725311ad7e5fa068263a7eb037917e0
Tim DeCillis <timdecillis@gmail.com>
2024-02-10 10:57:00 -0800

    added new test stub

--- client/src/components/App/App.test.js
+++ client/src/components/App/App.test.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { render } from "@testing-library/react";
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
 
 import App from "./App.jsx";
 import LandingPage from "../LandingPage/LandingPage.jsx";
@@ -11,7 +11,17 @@ describe("App Component", () => {
     // expect(document.querySelector(".container")).toBeTruthy();
     // expect(document.querySelector(".banner")).toBeTruthy();
     // expect(document.querySelector(".header")).toBeTruthy();
-    const header = getByText('Repertoire');
+    const header = getByText("Repertoire");
     expect(header).toBeInTheDocument();
+  });
+  it("calls the sign in function", () => {
+    const handleClick = jest.fn();
+    const { getByText } = render(
+      <Button label="Click me" onClick={handleClick} />
+    );
+    const button = getByText("Click me");
+
+    fireEvent.click(button);
+    expect(handleClick).toHaveBeenCalledTimes(1);
   });
 });

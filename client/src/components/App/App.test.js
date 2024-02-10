3bd54e3992a26f9a490f0829dcf26f1b9dc12ae5
Tim DeCillis <timdecillis@gmail.com>
2024-02-10 10:53:53 -0800

    added new assertion

--- client/src/components/App/App.test.js
+++ client/src/components/App/App.test.js
@@ -6,9 +6,11 @@ import LandingPage from "../LandingPage/LandingPage.jsx";
 
 describe("App Component", () => {
   it("includes the container, banner, and header", () => {
-    render(<App />);
-    expect(document.querySelector(".container")).toBeTruthy();
-    expect(document.querySelector(".banner")).toBeTruthy();
-    expect(document.querySelector(".header")).toBeTruthy();
+    const { getByText } = render(<App />);
+    // expect(document.querySelector(".container")).toBeTruthy();
+    // expect(document.querySelector(".banner")).toBeTruthy();
+    // expect(document.querySelector(".header")).toBeTruthy();
+    const header = getByText('Repertoire');
+    expect(header).toBeInTheDocument();
   });
 });

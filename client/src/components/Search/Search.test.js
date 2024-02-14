62cc4ac510130c2670ee6b1424d71ac16e1b1faa
Tim DeCillis <timdecillis@gmail.com>
2024-02-14 13:46:42 -0800

    added full test script

--- client/src/components/Search/Search.test.js
+++ client/src/components/Search/Search.test.js
@@ -3,15 +3,15 @@ import React, { useState } from "react";
 import { render, fireEvent, queryByText } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
-import Search from './Search.jsx'
+import Search from './Search.jsx';
 
-describe("Instrument Component", () => {
+describe("Search Component", () => {
   it("renders without throwing an error", () => {
     const props = {
       signedIn: true,
       choices: [],
       dashOpen: true,
     };
-    render(<Instrument {...props} />);
+    render(<Search {...props} />);
   });
 });

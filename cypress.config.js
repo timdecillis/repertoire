45e8dca3df9cdfdd2935ee49d7637743826f85fd
Tim DeCillis <timdecillis@gmail.com>
2024-02-14 17:24:19 -0800

    gave up on stupid cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  browsers: [{ name: "chrome" }],
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here

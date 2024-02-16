c36a2ab8ab5b592ffe38dbb145b126102d619571
Tim DeCillis <timdecillis@gmail.com>
2024-02-16 15:04:40 -0800

    added assertions successfully

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,8 @@
-describe('App spec', () => {
-  it('loads the home page', () => {
-    cy.visit('localhost:3000');
+describe("App spec", () => {
+  it("loads the home page", () => {
+    cy.visit("localhost:3000");
+    cy.get(".container").should("be.visible");
+    cy.get(".header").should("be.visible");
+    cy.get(".banner").should("be.visible");
   });
 });

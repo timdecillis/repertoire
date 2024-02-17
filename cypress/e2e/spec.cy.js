496aa77107aa9328c1f0f0a8c8430e5172f83db1
Tim DeCillis <timdecillis@gmail.com>
2024-02-16 19:46:26 -0800

    finished signIn e2e test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,15 +1,23 @@
 describe("App spec", () => {
   it("loads the home page", () => {
     const get = cy.get;
+    const contains = cy.contains;
     cy.visit("localhost:3000");
     get(".container").should("be.visible");
     get(".header").should("be.visible");
     get(".banner").should("be.visible");
     cy.title().should("eq", "Repertoire");
 
-    cy.contains("input", "Log In").click();
+    contains("input", "Log In").click();
     get(".error");
-    cy.contains("div", "Please enter your email");
-    cy.get("#email").type("user@email.com");
+    contains("div", "Please enter your email");
+
+    get("#email").type("user@email.com");
+    contains("input", "Log In").click();
+    contains("div", "Please enter your password");
+
+    get("#password").type("asdfasdfasdf{enter}");
+
+    contains("div", "Songbook");
   });
 });

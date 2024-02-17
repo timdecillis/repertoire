72a20407e5e1825634dd78b0b71f6de9fa2d11e6
Tim DeCillis <timdecillis@gmail.com>
2024-02-17 10:54:09 -0800

    completed check for password no email

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -11,12 +11,16 @@ describe("App spec", () => {
     get(".error");
     contains("div", "Please enter your email");
 
-    get("#email").type("user@email.com");
-    contains("input", "Log In").click();
-    contains("div", "Please enter your password");
-
+    get('#email').clear();
     get("#password").type("asdfasdfasdf{enter}");
+    contains("div", "Please enter your email");
 
-    contains("div", "Songbook");
+    // get("#email").type("user@email.com");
+    // contains("input", "Log In").click();
+
+    // get("#password").type("asdfasdfasdf{enter}");
+    // contains("div", "Songbook");
+
+
   });
 });

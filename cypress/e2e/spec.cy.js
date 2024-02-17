929c26c2cb2b07a4f0109f3cff481165b8fab22d
Tim DeCillis <timdecillis@gmail.com>
2024-02-16 19:35:03 -0800

    cleaned up test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,10 @@
 describe("App spec", () => {
   it("loads the home page", () => {
+    const get = cy.get;
     cy.visit("localhost:3000");
-    cy.get(".container").should("be.visible");
-    cy.get(".header").should("be.visible");
-    cy.get(".banner").should("be.visible");
+    get(".container").should("be.visible");
+    get(".header").should("be.visible");
+    get(".banner").should("be.visible");
     cy.title().should("eq", "Repertoire");
 
     cy.contains("input", "Log In").click();

8aae2460e2e8fe8effb7f28d7b114c3cf4548b06
Tim DeCillis <timdecillis@gmail.com>
2024-02-21 19:50:21 -0800

    added email input

--- cypress/e2e/newUser.cy.js
+++ cypress/e2e/newUser.cy.js
@@ -2,5 +2,8 @@ describe("App spec", () => {
   it("handles a new user correctly", () => {
     const { get, contains, visit, title, intercept, wait } = cy;
     visit("localhost:3000");
+    get("#email").type("newuser@email.com");
+    contains("input", "Log In").click();
+
   });
 });

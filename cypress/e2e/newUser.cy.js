dc7f5f92701d71d226f48c4f9cdd50aa4219e30e
Tim DeCillis <timdecillis@gmail.com>
2024-02-21 20:11:47 -0800

    updated email in test

--- cypress/e2e/newUser.cy.js
+++ cypress/e2e/newUser.cy.js
@@ -2,7 +2,7 @@ describe("App spec", () => {
   it("handles a new user correctly", () => {
     const { get, contains, visit, title, intercept, wait } = cy;
     visit("localhost:3000");
-    get("#email").type("newuser@email.com");
+    get("#email").type("thisnewuser@email.com");
     contains("input", "Log In").click();
     get("#password").type("asdfasdfasdf{enter}");
     get(".song").should("not.exist");

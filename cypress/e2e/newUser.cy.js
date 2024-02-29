471ee2e0423e1ace97fa9ccc2c7c1aa5dc50b90d
Tim DeCillis <timdecillis@gmail.com>
2024-02-28 17:11:41 -0800

    cleaned up test

--- cypress/e2e/newUser.cy.js
+++ cypress/e2e/newUser.cy.js
@@ -8,14 +8,13 @@ describe("App spec", () => {
     get(".song").should("not.exist");
 
     contains("span", "+Add Song").click();
-    // get('input[value="guitar"]').click();
-    // get('input[value="beginner"]').click();
-    // get(".search-input").type("Aerosmith{enter}");
-    // get(".choice").first().click();
+
     get('input[value="drums"]').click();
     get('input[value="advanced"]').click();
-    get(".search-input").type("Van Halen{enter}");
+
+    get(".search-input").type("Aerosmith{enter}");
     get(".choices").should("be.visible");
+    // get(".choice").first().click();
 
     intercept("POST", "/addSong").as("addSong");
     get(".choice").first().click();

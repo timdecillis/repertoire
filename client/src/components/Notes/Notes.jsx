c5de1f83774bcef3c783a43dff775771777621b9
Tim DeCillis <timdecillis@gmail.com>
2024-03-11 17:22:59 -0700

    passed notes down

--- client/src/components/Notes/Notes.jsx
+++ client/src/components/Notes/Notes.jsx
@@ -4,7 +4,7 @@ import { deleteSong, completeSong, handleNotes } from "../../lib.js";
 import Completed from "../Completed/Completed.jsx";
 import Draft from "../Draft/Draft.jsx";
 
-const Notes = ({ setDraftOpen, song, authUser, setSongs, handleSubmit }) => {
+const Notes = ({ setDraftOpen, song, authUser, setSongs, handleSubmit, notes }) => {
   const [draft, setDraft] = useState("");
   const [success, setSuccess] = useState(false);
 
--- client/src/components/SongCard/SongCard.jsx
+++ client/src/components/SongCard/SongCard.jsx
@@ -29,6 +29,7 @@ const SongCard = ({ song, authUser, setSongs, setDashOpen }) => {
             </div>
           ) : (
             <Notes
+              notes={notes}
               handleSubmit={handleSubmit}
               setSongs={setSongs}
               authUser={authUser}

8e61d95c9dfa6aba2fb1692759b454caa7df7056
Tim DeCillis <timdecillis@gmail.com>
2024-03-17 18:08:21 -0700

    commented out createUser inputs

--- client/src/components/CreateUser/CreateUser.jsx
+++ client/src/components/CreateUser/CreateUser.jsx
@@ -11,8 +11,8 @@ const CreateUser = ({
 }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [first, setFirst] = useState("");
-  const [last, setLast] = useState("");
+  // const [first, setFirst] = useState("");
+  // const [last, setLast] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -33,8 +33,8 @@ const CreateUser = ({
     <form onSubmit={handleSubmit} className="sign-in">
       <div className="inputs">
         <div className="first-input" htmlFor="email">Create Account</div>
-        <input onChange={(e) => setFirst(e.target.value)} placeholder="first name" className="input" id="firstName" type="text" name="firstName" />
-        <input onChange={(e) => setLast(e.target.value)} placeholder="last name" className="input" id="lastName" type="text" name="lastName" />
+        {/* <input onChange={(e) => setFirst(e.target.value)} placeholder="first name" className="input" id="firstName" type="text" name="firstName" />
+        <input onChange={(e) => setLast(e.target.value)} placeholder="last name" className="input" id="lastName" type="text" name="lastName" /> */}
         <input onChange={(e) => setEmail(e.target.value)} placeholder="email" className="input" id="email" type="text" name="email" />
         <input onChange={(e) => setPassword(e.target.value)} placeholder="password" className="input" id="password" type="password" name="password" />
         <input style={{borderStyle: 'solid', borderWidth: '.1em', backgroundColor: 'darkcyan', color: 'whitesmoke', marginBottom: '.7em'}} className="input" type="submit" value="Submit" />

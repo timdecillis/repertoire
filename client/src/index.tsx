649a29e8c0291d9276c7107685b8e1efde1b0349
Tim DeCillis <timdecillis@gmail.com>
2024-03-21 13:56:00 -0700

    created index.tsx

---
+++ client/src/index.tsx
@@ -0,0 +1,7 @@
+import {render, createRoot} from 'react-dom/client';
+import React from 'react';
+import App from './components/App/App.jsx';
+
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+root.render(<App />);

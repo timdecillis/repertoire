8fcf1d8b9b441ea5b2c865aaae7ea74457f8cecb
Tim DeCillis <timdecillis@gmail.com>
2024-03-22 16:33:58 -0700

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: path.join(__dirname, "/client/src/index.tsx"),
+  entry: path.join(__dirname, "/client/src/index.jsx"),
   output: {
     path: path.join(__dirname, "/client/dist"),
     filename: "bundle.js",

f808ef4e7b7bafeed2558eb7850c181a2eb277a1
Tim DeCillis <timdecillis@gmail.com>
2024-03-23 12:35:16 -0700

    cleaned up lib

--- client/src/lib.js
+++ client/src/lib.js
@@ -47,9 +47,5 @@ module.exports = {
   },
   createUser: (email, password) => {
     return axios.post("/users", { email: email, password: password });
-    // .then((response) => {
-    //   console.log('response in lib:', response);
-    // })
-    // .catch((err) => console.log(err));
   },
 };

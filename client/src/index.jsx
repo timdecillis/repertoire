75e678c0a725d60e62a6e95f5a78a46a7c97044e
Tim DeCillis <timdecillis@gmail.com>
2024-03-27 21:32:19 -0700

    added browser router

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,12 @@
-import {render, createRoot} from 'react-dom/client';
-import React from 'react';
-import App from './components/App/App.jsx';
+import { render, createRoot } from "react-dom/client";
+import React from "react";
+import App from "./components/App/App.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-const domNode = document.getElementById('root');
+const domNode = document.getElementById("root");
 const root = createRoot(domNode);
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);

7477a758a38aa71cac038ff27161fc9d744fdcbb
Tim DeCillis <timdecillis@gmail.com>
2024-03-29 19:36:08 -0700

    added new index

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,42 @@
-import { render, createRoot } from "react-dom/client";
-import React from "react";
-import App from "./components/App/App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
-const domNode = document.getElementById("root");
-const root = createRoot(domNode);
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <h1>Hello World</h1>
+        <Link to="about">About Us</Link>
+      </div>
+    ),
+  },
+  {
+    path: "about",
+    element: <div>About</div>,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );
+
+
+// import { render, createRoot } from "react-dom/client";
+// import React from "react";
+// import App from "./components/App/App.jsx";
+// import { BrowserRouter } from "react-router-dom";
+
+// const domNode = document.getElementById("root");
+// const root = createRoot(domNode);
+// root.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// );

e53907093119d6dfe23b9e361676ff42114e5762
Tim DeCillis <timdecillis@gmail.com>
2024-04-11 15:49:50 -0700

    switched to browser router

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,43 +1,43 @@
-import React, { useState } from "react";
-import { createRoot } from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
+// import React, { useState } from "react";
+// import { createRoot } from "react-dom/client";
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+//   Route,
+//   Link,
+// } from "react-router-dom";
 
-import App from "./components/App/App.jsx";
-import SignIn from "./components/SignIn/SignIn.jsx";
-import Header from "./components/Header/Header.jsx";
-import SongList from "./components/SongList/SongList.jsx";
-import CreateUser from "./components/CreateUser/CreateUser.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <div>
-        <Header />
-        <App />
-      </div>
-    ),
-  },
-]);
-
-createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
-);
-
-// import { render, createRoot } from "react-dom/client";
-// import React from "react";
 // import App from "./components/App/App.jsx";
-// import { BrowserRouter } from "react-router-dom";
+// import SignIn from "./components/SignIn/SignIn.jsx";
+// import Header from "./components/Header/Header.jsx";
+// import SongList from "./components/SongList/SongList.jsx";
+// import CreateUser from "./components/CreateUser/CreateUser.jsx";
 
-// const domNode = document.getElementById("root");
-// const root = createRoot(domNode);
-// root.render(
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: (
+//       <div>
+//         <Header />
+//         <App />
+//       </div>
+//     ),
+//   },
+// ]);
+
+// createRoot(document.getElementById("root")).render(
+//   <RouterProvider router={router} />
 // );
+
+import { render, createRoot } from "react-dom/client";
+import React from "react";
+import App from "./components/App/App.jsx";
+import { BrowserRouter } from "react-router-dom";
+
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);

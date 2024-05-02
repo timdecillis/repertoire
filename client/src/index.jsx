2c61b63c3429f77eb5a70eed55698dda24f6d65f
timdecillis <timdecillis@gmail.com>
2024-05-01 17:33:49 -0700

    added App to root route

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,14 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import React from "react";
 import App from "./components/App/App.jsx";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <div>This is an error</div>,
+  },
+]);
+
 createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
 );

89daff03794398ede180c0d6a2ad9fda24baf2c1
timdecillis <timdecillis@gmail.com>
2024-05-01 17:31:57 -0700

    rendered provider

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,6 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import React from "react";
 import App from "./components/App/App.jsx";
 
-const domNode = document.getElementById("root");
-const root = createRoot(domNode);
-root.render(<App />);
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);

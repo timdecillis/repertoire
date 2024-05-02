4e638770856488b86f77bf13e2c7227dcf3739b3
timdecillis <timdecillis@gmail.com>
2024-05-01 17:59:44 -0700

    moved image

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,19 +7,28 @@ import CreateUser from "./components/CreateUser/CreateUser.jsx";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: (
+      <>
+        <App />
+        <img
+          className="painting"
+          src="https://media.istockphoto.com/id/1220009855/vector/a-young-man-playing-guitar-at-home-guitarist-musician-is-sitting-in-quarantine-alone-flat.jpg?s=612x612&w=0&k=20&c=p9bINE_TxSd4G4VRkvaHffh5vZjUdVUQvXGcqBlDrSs="
+          alt="a man playing guitar with his cat"
+        ></img>
+      </>
+    ),
     errorElement: <div>This is an error</div>,
     children: [
       {
         path: "create",
-        element: <CreateUser/>
-      }
-    ]
+        element: <CreateUser />,
+      },
+    ],
   },
   {
     path: "create",
-    element: <CreateUser/>
-  }
+    element: <CreateUser />,
+  },
 ]);
 
 createRoot(document.getElementById("root")).render(

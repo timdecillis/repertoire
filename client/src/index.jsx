49f38163ffc3b43b43e31f28170f468b18a9d4b2
timdecillis <timdecillis@gmail.com>
2024-05-01 17:30:09 -0700

    added imports

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,4 +1,5 @@
 import { render, createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import React from "react";
 import App from "./components/App/App.jsx";
 

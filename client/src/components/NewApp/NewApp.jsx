02acd5bc9b109a2982d58bcfed9be3d683356b58
timdecillis <timdecillis@gmail.com>
2024-05-03 10:47:51 -0700

    fixed img

--- client/src/components/NewApp/NewApp.jsx
+++ client/src/components/NewApp/NewApp.jsx
@@ -2,13 +2,17 @@ import React from "react";
 
 const NewApp = () => {
   return (
-    <div className="banner">
-      <div className="header">Repertoire</div>
-      <img
-        className="painting"
-        src="https://media.istockphoto.com/id/1220009855/vector/a-young-man-playing-guitar-at-home-guitarist-musician-is-sitting-in-quarantine-alone-flat.jpg?s=612x612&w=0&k=20&c=p9bINE_TxSd4G4VRkvaHffh5vZjUdVUQvXGcqBlDrSs="
-        alt="a man playing guitar with his cat"
-      ></img>
+    <div className="container">
+      <div className="landing">
+        <div className="banner">
+          <div className="header">Repertoire</div>
+        </div>
+        <img
+          className="painting"
+          src="https://media.istockphoto.com/id/1220009855/vector/a-young-man-playing-guitar-at-home-guitarist-musician-is-sitting-in-quarantine-alone-flat.jpg?s=612x612&w=0&k=20&c=p9bINE_TxSd4G4VRkvaHffh5vZjUdVUQvXGcqBlDrSs="
+          alt="a man playing guitar with his cat"
+        ></img>
+      </div>
     </div>
   );
 };

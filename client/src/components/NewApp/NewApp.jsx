afb8dd913d070025e3954ba6dbc404f433c4ccd1
timdecillis <timdecillis@gmail.com>
2024-05-03 10:42:56 -0700

    created stub for newapp

---
+++ client/src/components/NewApp/NewApp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewApp = () => {
+  return (
+    <div className="banner">
+      <div className="header">Repertoire</div>
+    </div>
+  );
+};
+
+export default NewApp;

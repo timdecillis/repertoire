01fb66850cc67c62f2aba927fdbca19447dfa9c6
timdecillis <timdecillis@gmail.com>
2024-05-03 10:43:57 -0700

    added img to newapp

--- client/src/components/NewApp/NewApp.jsx
+++ client/src/components/NewApp/NewApp.jsx
@@ -4,6 +4,11 @@ const NewApp = () => {
   return (
     <div className="banner">
       <div className="header">Repertoire</div>
+      <img
+        className="painting"
+        src="https://media.istockphoto.com/id/1220009855/vector/a-young-man-playing-guitar-at-home-guitarist-musician-is-sitting-in-quarantine-alone-flat.jpg?s=612x612&w=0&k=20&c=p9bINE_TxSd4G4VRkvaHffh5vZjUdVUQvXGcqBlDrSs="
+        alt="a man playing guitar with his cat"
+      ></img>
     </div>
   );
 };
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -11,11 +11,7 @@ const router = createBrowserRouter([
     element: (
       <>
         <Header />
-        <img
-          className="painting"
-          src="https://media.istockphoto.com/id/1220009855/vector/a-young-man-playing-guitar-at-home-guitarist-musician-is-sitting-in-quarantine-alone-flat.jpg?s=612x612&w=0&k=20&c=p9bINE_TxSd4G4VRkvaHffh5vZjUdVUQvXGcqBlDrSs="
-          alt="a man playing guitar with his cat"
-        ></img>
+
       </>
     ),
     errorElement: <div>This is an error</div>,

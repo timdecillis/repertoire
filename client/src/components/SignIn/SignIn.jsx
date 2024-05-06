690b270fecc748efa1d7fa7585b7ea24b65de6e6
timdecillis <timdecillis@gmail.com>
2024-05-05 21:24:29 -0700

    imported navigate

--- client/src/components/SignIn/SignIn.jsx
+++ client/src/components/SignIn/SignIn.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import { getSongs } from "../../lib.js";
 import Error from "../Error/Error.jsx";

bc12833ebef79d3593a86ca90dc2fe2eaf6818de
timdecillis <timdecillis@gmail.com>
2024-05-13 14:20:11 -0700

    updated props

--- client/src/components/Instrument/Instrument.jsx
+++ client/src/components/Instrument/Instrument.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const Instrument = ({ instrument, setter, options, title }) => {
+const RadioOptions = ({ setter, options, title }) => {
   return (
     <div className="instrument">
       <div className="dash-option">Select {title}</div>
@@ -22,29 +22,9 @@ const Instrument = ({ instrument, setter, options, title }) => {
             </div>
           );
         })}
-        {/* <input
-          onClick={(e) => setInstrument(e.target.value)}
-          type="radio"
-          name="instrument"
-          value="drums"
-          className="radio"
-        ></input>
-        <label className="radio-button" htmlFor="instrument">
-          Drums
-        </label>
-        <input
-          onClick={(e) => setInstrument(e.target.value)}
-          type="radio"
-          name="instrument"
-          value="piano"
-          className="radio"
-        ></input>
-        <label className="radio-button" htmlFor="instrument">
-          Piano
-        </label> */}
       </div>
     </div>
   );
 };
 
-export default Instrument;
+export default RadioOptions;

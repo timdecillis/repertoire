ab386be8ab190862f1102dbe0c17e33dd7501379
timdecillis <timdecillis@gmail.com>
2024-05-14 19:46:22 -0700

    added sign out button

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -4,16 +4,14 @@ const Header = () => {
   return (
     <div className="banner">
       <div className="header">Repertoire</div>
-      {/* {signedIn && (
-        <div>
-          <span onClick={handleAccount} className="sign-out-button">
-            Account
-          </span>
-          <span onClick={handleSignOut} className="sign-out-button">
-            Sign Out
-          </span>
-        </div>
-      )} */}
+      <div>
+        <span onClick={handleAccount} className="sign-out-button">
+          Account
+        </span>
+        <span onClick={handleSignOut} className="sign-out-button">
+          Sign Out
+        </span>
+      </div>
     </div>
   );
 };

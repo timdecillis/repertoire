c4f2cf42e3dfb21a8995f9707d798ee4aa787d78
timdecillis <timdecillis@gmail.com>
2024-06-08 16:53:42 -0700

    created new server route

--- server/app.js
+++ server/app.js
@@ -1,20 +1,30 @@
-const express = require('express');
-const morgan = require('morgan');
-const router = require('express').Router();
+const express = require("express");
+const morgan = require("morgan");
+const router = require("express").Router();
 const app = express();
 
-const {findBand, getSongs, addSong, deleteSong, updateSong, updateNotes, createUser} = require('./controllers.js');
+const {
+  findBand,
+  getSongs,
+  addSong,
+  deleteSong,
+  updateSong,
+  updateNotes,
+  createUser,
+  updateUser
+} = require("./controllers.js");
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.static('client/dist'));
-app.use('/', router);
-router.post('/findBand', findBand);
-router.post('/users', createUser);
-router.post('/addSong', addSong);
-router.get('/getSongs', getSongs);
-router.delete('/deleteSong', deleteSong);
-router.put('/updateSong', updateSong);
-router.put('/updateNotes', updateNotes);
+app.use(express.static("client/dist"));
+app.use("/", router);
+router.post("/findBand", findBand);
+router.post("/users", createUser);
+router.post("/addSong", addSong);
+router.get("/getSongs", getSongs);
+router.delete("/deleteSong", deleteSong);
+router.put("/updateSong", updateSong);
+router.put("/updateNotes", updateNotes);
+router.post("/updateUser", updateUser);
 
 module.exports = app;

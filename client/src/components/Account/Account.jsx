c50d727fe1c0d937bfc6c514e1ed1e6916176c5d
timdecillis <timdecillis@gmail.com>
2024-06-08 16:40:12 -0700

    added event handlers

--- client/src/components/Account/Account.jsx
+++ client/src/components/Account/Account.jsx
@@ -3,15 +3,27 @@ import React, { useState } from "react";
 const Account = () => {
   const [emailInput, setEmailInput] = useState("");
   const [passwordInput, setInput] = useState("");
+
+  const handleEmailSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.value);
+  };
+  const handlePasswordSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.value);
+  };
   return (
     <div>
       <h2>Account Options</h2>
       <h4>Change email</h4>
-      <input onChange={(e) => setEmailInput(e.target.value)}/>
+      <form>
+        <input onChange={(e) => setEmailInput(e.target.value)} />
+        <input type="submit" />
+      </form>
       <div>{emailInput}</div>
       <h4>Change password</h4>
       <div>{passwordInput}</div>
-      <input/>
+      <input />
     </div>
   );
 };

0fef71031ed667211cea6cd43dd9f927ba82d03f
timdecillis <timdecillis@gmail.com>
2024-06-10 14:54:49 -0700

    added user to NewApp

--- client/src/components/NewApp/NewApp.jsx
+++ client/src/components/NewApp/NewApp.jsx
@@ -1,24 +1,30 @@
+import { useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 
+import { DataContext } from "../../context.js";
+
 const NewApp = () => {
+  const { user } = useContext(DataContext);
   return (
     <div className="container">
       <div className="landing">
         <div className="banner">
           <div className="header">Repertoire</div>
-          <Link
-            to={"account"}
-            className="input"
-            style={{
-              textAlign: "center",
-              textDecoration: "underline",
-              fontSize: ".7em",
-              cursor: "pointer",
-              color: "black",
-            }}
-          >
-            Account
-          </Link>
+          {user && (
+            <Link
+              to={"account"}
+              className="input"
+              style={{
+                textAlign: "center",
+                textDecoration: "underline",
+                fontSize: ".7em",
+                cursor: "pointer",
+                color: "black",
+              }}
+            >
+              Account
+            </Link>
+          )}
         </div>
         <Outlet />
       </div>

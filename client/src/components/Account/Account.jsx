3e29c68cc15b621f29bea65ff5d46314ce805dd1
timdecillis <timdecillis@gmail.com>
2024-06-10 15:00:21 -0700

    updated handler

--- client/src/components/Account/Account.jsx
+++ client/src/components/Account/Account.jsx
@@ -9,25 +9,25 @@ const Account = () => {
 
   const { user } = useContext(DataContext);
 
-  const handleEmailSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     updateUser(user, "email", emailInput);
   };
   const handlePasswordSubmit = (e) => {
     e.preventDefault();
-    updateUser("password", passwordInput);
+    updateUser(user, "password", passwordInput);
   };
   return (
     <div>
       <h2>Account Options</h2>
       <h4>Change email</h4>
       <form onSubmit={handleEmailSubmit}>
-        <input onChange={(e) => setEmailInput(e.target.value)} />
+        <input name="email" onChange={handleSubmit} />
         <input type="submit" value="Submit" />
       </form>
       <h4>Change password</h4>
       <form onSubmit={handlePasswordSubmit}>
-        <input onChange={(e) => setPasswordInput(e.target.value)} />
+        <input name="password" onChange={handleSubmit} />
         <input type="submit" value="Submit" />
       </form>
     </div>

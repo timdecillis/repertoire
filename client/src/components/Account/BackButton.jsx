ed00569bc65c84c567c0aa6b8746d07b87c23020
timdecillis <timdecillis@gmail.com>
2024-06-16 13:04:13 -0700

    completed button

--- client/src/components/Account/BackButton.jsx
+++ client/src/components/Account/BackButton.jsx
@@ -1,20 +1,3 @@
-export default BackButton = () => {
-  const backButton = ({ handler }) => <button onClick={handler}>Back</button>;
-
-  return (
-    <div>
-      <h2>Account Options</h2>
-      {!inputOpen && (
-        <>
-          <button onClick={() => setInputOpen("email")}>Change email</button>
-          <button onClick={() => setInputOpen("password")}>
-            Change password
-          </button>
-        </>
-      )}
-      {inputOpen === "email" && changeForm("email", backButton)}
-      {inputOpen === "password" && changeForm("password", backButton)}
-      {feedback && <div>{feedback}</div>}
-    </div>
-  );
-};
+export default BackButton = ({ handler }) => (
+  <button onClick={handler}>Back</button>
+);

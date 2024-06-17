c9a6abebcc1997f30aaaee5c9d6ce93d052f4309
timdecillis <timdecillis@gmail.com>
2024-06-17 14:56:12 -0700

    added ChangeForm for password

--- client/src/components/Account/Account.jsx
+++ client/src/components/Account/Account.jsx
@@ -49,6 +49,9 @@ const Account = () => {
       {inputOpen === "email" && (
         <ChangeForm type="email" inputs={inputs} handler={handleSubmit} button={<BackButton handler={handleBack} />} />
       )}
+      {inputOpen === "password" && (
+        <ChangeForm type="password" inputs={inputs} handler={handleSubmit} button={<BackButton handler={handleBack} />} />
+      )}
 
       {feedback && <div>{feedback}</div>}
     </div>
